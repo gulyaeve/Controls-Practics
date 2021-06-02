ca7292bd50af052623c703169fd28a42c1859e15
gulyaeve <gulyaev.e@icloud.com>
2021-06-02 16:23:39 +0300

    Initial Commit

---
+++ Controls Practics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Controls Practics
+//
+//  Created by Евгений Гуляев on 02.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
